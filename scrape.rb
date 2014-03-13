8b8fa2b860213b6cdfa968a088fd3cf869d5c3fc
Billy Rhoades <billyarhoades@gmail.com>
2014-03-13 16:49:46 -0500

    Moved column naming to a hash schema. Added some comments and some addtional statistics.
    Moving the columns to the new schema simplified the csv output.

--- scrape.rb
+++ scrape.rb
@@ -13,36 +13,65 @@ accessories_url = "http://battlelog.battlefield.com/bf4/warsawWeaponAccessoriesP
 output = "#{outputName}.csv"
 
 weaponStatsJSON = JSON.parse(open(weapons_url).read)
-weapons = Hash.new
+weapons = Array.new
 
 
 Parallel.each(weaponStatsJSON['data']['mainWeaponStats'], :in_threads => 20, :in_processes => 2) do |wepStats|
-    name = wepStats['slug']
-    guid = wepStats['guid']
-    kills = wepStats['kills']
-    category = wepStats['category']
+  guid = wepStats['guid']
 
-    accessoriesJSON = JSON.parse(open(accessories_url + "#{guid}/").read)
+  accessoriesJSON = JSON.parse(open(accessories_url + "#{guid}/").read)
 
-    completed = 0
-    totalAccessories = accessoriesJSON['data']['statsItemUnlocks'].length
-    if totalAccessories > 0  
-        accessoriesJSON['data']['statsItemUnlocks'].each do |accessory|
-            if accessory['unlockedBy']['completed']
-                completed += 1
-            end
+  # print JSON.pretty_generate( wepStats ), "\n\n\n"
+
+  completed = 0
+  totalAccessories = accessoriesJSON['data']['statsItemUnlocks'].length
+  totalUnlockableAccessories = 0
+  totUnlockableAccessUnlocked = 0
+  if totalAccessories > 0  
+    accessoriesJSON['data']['statsItemUnlocks'].each do |accessory|
+      # This includes battlepack unlocked accessories
+      if accessory['unlockedBy']['completed']
+        if accessory['unlockedBy']['valueNeeded'] != nil 
+          totUnlockableAccessUnlocked += 1
         end
-
-        weapons[name] = {'kills' => kills, 'unlocked accessories' => completed, 'total accessories' => totalAccessories, 'category' => category}
+        completed += 1
+      end
+      if accessory['unlockedBy']['valueNeeded'] != nil
+        totalUnlockableAccessories += 1 
+      end
     end
+  end
+
+  # For our columns, these map the JSON names to the column names. Text means it's going to copy
+  #   from wepStats directly.
+  cols = { 
+    'Name'                         => 'slug',
+    'Kills'                        => 'kills',
+    'Category'                     => 'category',
+    'Unlocked Accessories'         => completed,
+    'Total Accessories'            => totalAccessories,
+    'Total Unlockable Accessories' => totalUnlockableAccessories,
+    'Total Unlockable Unlocked Accessories' => totUnlockableAccessUnlocked
+  }
+
+  # Hack for capital names:
+  wepStats[cols['Name']].swapcase!
+
+  # Map our values over.
+  cols.each do |i, val|
+    if val.is_a? String
+      cols[i] = wepStats[val]
+    end
+  end
+
+  weapons << cols
 end
 
 
 CSV.open(output, 'w') do |csv|
-    csv << ['name', 'percent complete'].concat(weapons.values[0].keys)
-    
-    weapons.each do |name, hash|
-        percent = hash['unlocked accessories'].fdiv(hash['total accessories']).round(3)
-        csv << [name, percent].concat(hash.values)
-    end
+  csv << weapons.first.keys
+
+  weapons.each do |hash|
+    csv << hash.values
+  end
 end
